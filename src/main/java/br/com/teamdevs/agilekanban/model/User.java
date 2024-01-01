be93ecc4e231fbeb4f4dfb63795a313159a37d90
andre.borba <dev_andre.filipe@outlook.com>
2024-01-01 19:29:13 -0300

    feat: User

---
+++ src/main/java/br/com/teamdevs/agilekanban/model/User.java
@@ -0,0 +1,41 @@
+package br.com.teamdevs.agilekanban.model;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+@Document(collection = "allData")
+public class User {
+    @Id
+    private String id;
+
+    @Field(name = "username")
+    private String username;
+
+    @Field(name = "password")
+    private String password;
+
+    @Field(name = "email")
+    private String email;
+
+    @Field(name = "role")
+    private String role;
+
+    @Field(name = "projects")
+    private List<String> projects;
+
+    @Field(name = "createdTasks")
+    private List<String> createdTasks;
+
+    @Field(name = "assignedTasks")
+    private List<String> assignedTasks;
+}
