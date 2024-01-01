87a717ada01cb9e8e5b5528d9320868844b19e78
andre.borba <dev_andre.filipe@outlook.com>
2024-01-01 09:16:29 -0300

    feat: initial commit project

---
+++ src/main/java/br/com/teamdevs/agilekanban/AgileKanbanApplication.java
@@ -0,0 +1,13 @@
+package br.com.teamdevs.agilekanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileKanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileKanbanApplication.class, args);
+	}
+
+}
