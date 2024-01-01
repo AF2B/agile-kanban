c77a7d9e0d083ae830614163a85aa624f25c90b8
andre.borba <dev_andre.filipe@outlook.com>
2024-01-01 19:28:57 -0300

    feat: user validation

---
+++ src/main/java/br/com/teamdevs/agilekanban/services/ValidationsController/UserValidations/UserValidation.java
@@ -0,0 +1,8 @@
+package br.com.teamdevs.agilekanban.services.ValidationsController.UserValidations;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserValidation {
+    
+}
