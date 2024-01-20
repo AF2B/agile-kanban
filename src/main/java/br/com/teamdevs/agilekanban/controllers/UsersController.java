e874403c747a832b2653d93a241fb55e18257deb
andre.borba <dev_andre.filipe@outlook.com>
2024-01-20 18:40:15 -0300

    refactor: alterado UsersRequestDTO para User

--- src/main/java/br/com/teamdevs/agilekanban/controllers/UsersController.java
+++ src/main/java/br/com/teamdevs/agilekanban/controllers/UsersController.java
@@ -1,34 +1,34 @@
 package br.com.teamdevs.agilekanban.controllers;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.apache.catalina.connector.Response;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.teamdevs.agilekanban.dto.UserRequestDTO;
 import br.com.teamdevs.agilekanban.dto.UserResponseDTO;
-import br.com.teamdevs.agilekanban.exception.CustomException;
-import br.com.teamdevs.agilekanban.exception.UserNotFoundException;
 import br.com.teamdevs.agilekanban.model.User;
 import br.com.teamdevs.agilekanban.services.UserService;
 import br.com.teamdevs.agilekanban.services.ValidationsController.UserValidations.UserValidationPropertiesService;
-import jakarta.validation.Valid;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/v1")
 @Validated
+@Tag(name = "Usuários", description = "Endpoints para gerenciar usuários")
 public class UsersController {
     private UserService userService;
     private UserValidationPropertiesService userValidationPropertiesService;
@@ -38,25 +38,56 @@ public class UsersController {
         this.userValidationPropertiesService = userValidationPropertiesServiceInj;
     }
 
+    @Operation(
+        summary = "Verificar o status da aplicação",
+        description = "Endpoint para verificar o status da aplicação."
+    )
     @GetMapping("/health-check")
     public ResponseEntity<HttpStatus> healthCheck() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(
+       summary = "Listar todos os usuários",
+       description = "Endpoint para listar todos os usuários cadastrados."
+   )
+   @ApiResponses(value = {
+       @ApiResponse(responseCode = "200", description = "Lista de usuários", content = {
+           @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UserResponseDTO.class)))
+       })
+   })
    @GetMapping("/users")
    public ResponseEntity<UserResponseDTO> GetAllUsers() {
         var data = userService.findAll();
-
-        if (data == null || data.isEmpty()) {
-            throw new CustomException(HttpStatus.NOT_FOUND.value(), "not found");
-        }
         UserResponseDTO response = new UserResponseDTO(data);
 
         return ResponseEntity.status(200).body(response);
    }
 
+   @Operation(
+        summary = "Obter informações de um usuário",
+        description = "Endpoint para obter informações de um usuário pelo ID."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Usuário encontrado"),
+        @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> findById(@PathVariable String id) {
+        User data = userService.find(id);
+        return ResponseEntity.ok(data);
+    }
+
+   @Operation(
+       summary = "Criar um novo usuário",
+       description = "Endpoint para criar um novo usuário."
+   )
+   @ApiResponses(value = {
+       @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso"),
+       @ApiResponse(responseCode = "400", description = "Requisição inválida")
+   })
    @PostMapping("/users")
-    public ResponseEntity<HttpStatus> CreateUser(@RequestBody UserRequestDTO user) {
+    public ResponseEntity<HttpStatus> CreateUser(@RequestBody User user) {
         // if (user.getUsername() == null && user.getUsername().isEmpty()) {
         //     throw new CustomException(HttpStatus.BAD_REQUEST.value(), "blabla...");
         // }
@@ -66,20 +97,31 @@ public class UsersController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(
+        summary = "Atualizar um usuário",
+        description = "Endpoint para atualizar um usuário existente."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Usuário atualizado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
     @PutMapping("/users/{id}")
-    public ResponseEntity<User> updateUser(@RequestParam Long id, UserRequestDTO request) {
-        userValidationPropertiesService.validateUserProperties(request.getEmail(), request.getUsername(), request.getPassword());
+    public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody User requestDTO) {
+        User updatedUser = userService.update(id, requestDTO);
+        return ResponseEntity.ok(updatedUser);
+    }
 
-        User user = userService.update(id);
-
-        boolean isUserNull = (user != null ? true : false);
-
-        if (isUserNull) {
-            return ResponseEntity.ok(user);
-        } else {
-            throw new UserNotFoundException(HttpStatus.NOT_FOUND.value(), "not found user....");
-        }
+    @Operation(
+        summary = "Remover um usuário",
+        description = "Endpoint para remover um usuário pelo ID."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Usuário removido com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> remove(@PathVariable String id) {
+        userService.remove(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
-
-
