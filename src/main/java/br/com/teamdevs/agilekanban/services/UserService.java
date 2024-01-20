8e4116453d85e17761deb41e24772771f2758a51
andre.borba <dev_andre.filipe@outlook.com>
2024-01-20 18:42:29 -0300

    feat: adicionado validacoes de se null

--- src/main/java/br/com/teamdevs/agilekanban/services/UserService.java
+++ src/main/java/br/com/teamdevs/agilekanban/services/UserService.java
@@ -1,13 +1,12 @@
 package br.com.teamdevs.agilekanban.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import br.com.teamdevs.agilekanban.dto.UserRequestDTO;
-import br.com.teamdevs.agilekanban.dto.UserResponseDTO;
+import br.com.teamdevs.agilekanban.exception.CustomException;
 import br.com.teamdevs.agilekanban.model.User;
 import br.com.teamdevs.agilekanban.repository.UserRepository;
 
@@ -16,13 +15,35 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public void save(UserRequestDTO user) {
+    public void save(User user) {
         repository.save(user);
     }
 
     public List<User> findAll() {
         var data = repository.findAll();
         
+        if (data == null || data.isEmpty()) {
+            throw new CustomException(HttpStatus.NOT_FOUND.value(), "Users not found.");
+        }
+
         return data;
+    }
+
+    public User update(String id, User request) {
+        return repository.update(id, request);
+    }
+
+    public User find(String id) {
+        User data = repository.findById(id);
+
+        if (data == null) {
+            throw new CustomException(HttpStatus.BAD_REQUEST.value(), "User not found.");
+        }
+
+        return data;
+    }
+
+    public void remove(String id) {
+        repository.delete(id);
     }
 }
