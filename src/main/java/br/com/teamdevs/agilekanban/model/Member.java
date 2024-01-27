79d5531730b9170e8da421e633bb73eb5105a3fd
andre.borba <dev_andre.filipe@outlook.com>
2024-01-27 01:54:20 -0300

    fix: correcao de campos opcionais

--- src/main/java/br/com/teamdevs/agilekanban/model/Member.java
+++ src/main/java/br/com/teamdevs/agilekanban/model/Member.java
@@ -3,9 +3,11 @@ package br.com.teamdevs.agilekanban.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Member {
     private String userId;
