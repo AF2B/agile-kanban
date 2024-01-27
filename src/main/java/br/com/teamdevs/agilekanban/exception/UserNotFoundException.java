b25109b622468cb96c15bfe7a5515e5c51825285
andre.borba <dev_andre.filipe@outlook.com>
2024-01-27 01:54:09 -0300

    refactor: usernotfoundexception atualizado

--- src/main/java/br/com/teamdevs/agilekanban/exception/UserNotFoundException.java
+++ src/main/java/br/com/teamdevs/agilekanban/exception/UserNotFoundException.java
@@ -4,12 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class UserNotFoundException extends RuntimeException {
-    private final int httpStatus;
-    private final String message;
-
-    public UserNotFoundException(int httpStatus, String message) {
+    public UserNotFoundException(String message) {
         super(message);
-        this.httpStatus = httpStatus;
-        this.message = message;
     } 
 }
