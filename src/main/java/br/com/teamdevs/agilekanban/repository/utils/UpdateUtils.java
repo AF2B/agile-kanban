62112ff5179c33b87145c40122dd9206c70af8ef
andre.borba <dev_andre.filipe@outlook.com>
2024-01-27 02:47:59 -0300

    feat: classe criada para tratar metodos de update em banco

---
+++ src/main/java/br/com/teamdevs/agilekanban/repository/utils/UpdateUtils.java
@@ -0,0 +1,31 @@
+package br.com.teamdevs.agilekanban.repository.utils;
+
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.lang.NonNull;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public final class UpdateUtils {
+    public static void updateIfPresent(@NonNull Update update, @NonNull String key, Object value) {
+        Optional.ofNullable(value)
+                .ifPresent(v -> update.set(key, v));
+    }
+
+    public static void updateEnumIfPresent(@NonNull Update update, @NonNull String key, Enum<?> value) {
+        Optional.ofNullable(value)
+                .ifPresent(v -> update.set(key, v.name()));
+    }
+
+    public static void updateDateIfPresent(@NonNull Update update, @NonNull String key, LocalDate date) {
+        Optional.ofNullable(date)
+                .ifPresent(d -> update.set(key, d));
+    }
+
+    public static void updateListIfPresent(@NonNull Update update, @NonNull String key, List<?> list) {
+        Optional.ofNullable(list)
+                .filter(l -> !l.isEmpty())
+                .ifPresent(l -> update.set(key, l));
+    }
+}
